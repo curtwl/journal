3bc887983eec04491d5a484b175d370ad842e5e0
Curt <curt.warner@gmail.com>
2023-07-25 20:28:31 -0500

    Fix issues from merge

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -13,18 +13,3 @@ const info = (...params) => {
   module.exports = {
     info, error
   }
-const info = (...params) => {
-    if (process.env.NODE_ENV !== 'test') { 
-      console.log(...params)
-    }
-  }
-  
-  const error = (...params) => {
-    if (process.env.NODE_ENV !== 'test') { 
-      console.error(...params)
-    }
-  }
-  
-  module.exports = {
-    info, error
-  }
